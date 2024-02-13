c2a0dda38c9ce9daad417f28df9ff0c2521f17bf
Stephen Melnicki <stephen@stephenmelnicki.com>
2024-02-13 09:04:29 -0800

    renaming import to pirsch-sdk

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export type { FreshContext, Plugin } from "$fresh/server.ts";
-export { type PirschHit, PirschNodeApiClient } from "pirsch";
+export { type PirschHit, PirschNodeApiClient } from "pirsch-sdk";
 export { delay } from "$std/async/mod.ts";

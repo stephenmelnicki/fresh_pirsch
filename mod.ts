95baf62a8f0e5e43e3075afd73de27f0eb7a0375
Stephen Melnicki <stephen@stephenmelnicki.com>
2024-02-12 22:25:12 -0800

    removing plugin module in favor of declaring in mod.ts

--- mod.ts
+++ mod.ts
@@ -1,2 +1,34 @@
+import { type Plugin } from "$fresh/server.ts";
+
+import { createReporter } from "./src/reporter.ts";
+import { PirschPluginOptions } from "./src/types.ts";
+
 export type { PirschPluginOptions } from "./src/types.ts";
-export { default } from "./src/plugin.ts";
+
+export default function pirschPlugin(options: PirschPluginOptions): Plugin {
+  const report = createReporter(options);
+
+  return {
+    name: "pirsch-plugin",
+    middlewares: [{
+      path: "/",
+      middleware: {
+        handler: async (req, ctx) => {
+          let res;
+          let err;
+
+          try {
+            res = await ctx.next();
+          } catch (error) {
+            err = error;
+            throw err;
+          } finally {
+            report(req, ctx);
+          }
+
+          return res;
+        },
+      },
+    }],
+  };
+}

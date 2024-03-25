50627580d8420b905c3a93fe66f9588fc549e8a6
Stephen Melnicki <stephen@stephenmelnicki.com>
2024-03-24 20:31:13 -0700

    updating deps to fresh 1.6.5

--- deps.ts
+++ deps.ts
@@ -1,12 +1,12 @@
 export type {
   FreshContext,
   Plugin,
-} from "https://deno.land/x/fresh@1.6.3/server.ts";
+} from "https://deno.land/x/fresh@1.6.5/server.ts";
 
 export {
   type PirschHit,
   PirschNodeApiClient,
   type Protocol,
-} from "npm:pirsch-sdk@2.5.0";
+} from "npm:pirsch-sdk@2.6.0";
 
 export { delay } from "https://deno.land/std@0.211.0/async/mod.ts";
